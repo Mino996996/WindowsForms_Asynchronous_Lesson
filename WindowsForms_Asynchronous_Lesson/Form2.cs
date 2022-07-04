d4b182a165183a4d5484a21e00a957a9e444ef25
minoru akahori <minoruaka@gmail.com>
2022-07-04 16:21:31 +0900

    update: Async/Awaitの書き方例を追加

---
+++ WindowsForms_Asynchronous_Lesson/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Asynchronous_Lesson
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = await Task.Run(() => GetData());
+            MessageBox.Show("完了");
+        }
+
+        private List<DTO> GetData()
+        {
+            var result = new List<DTO>();
+            for (int i = 0; i < 5; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                result.Add(new DTO(i.ToString(), "Name" + i));
+            }
+            return result;
+        }
+    }
+}
