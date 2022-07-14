91ec294821b17cdca5b9de9685075ff347ae264b
minoru akahori <minoruaka@gmail.com>
2022-07-14 16:29:39 +0900

    update(Form1.cs): add backgroundWorker to use async behaving

--- WindowsForms_Asynchronous_Lesson/Form1.Designer.cs
+++ WindowsForms_Asynchronous_Lesson/Form1.Designer.cs
@@ -32,6 +32,8 @@ namespace WindowsForms_Asynchronous_Lesson
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,11 +66,29 @@ namespace WindowsForms_Asynchronous_Lesson
             this.dataGridView1.Size = new System.Drawing.Size(496, 213);
             this.dataGridView1.TabIndex = 2;
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
@@ -85,6 +105,8 @@ namespace WindowsForms_Asynchronous_Lesson
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsForms_Asynchronous_Lesson/Form1.cs
+++ WindowsForms_Asynchronous_Lesson/Form1.cs
@@ -12,6 +12,8 @@ namespace WindowsForms_Asynchronous_Lesson
 {
     public partial class Form1 : Form
     {
+        static List<DTO> m_result = new List<DTO>();
+        static int m_DoWorkerFlag = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,26 +21,82 @@ namespace WindowsForms_Asynchronous_Lesson
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Task と context を使うとPromise.thenのような非同期処理の書き方が出来る
-            var context = TaskScheduler.FromCurrentSynchronizationContext();
-            Task.Run(() => GetData()).ContinueWith(x=>
-            {
-                // UIスレッド上で行う処理
-                dataGridView1.DataSource = x.Result; //x.Result はGetData()の返り値
-                MessageBox.Show("完了");
+            //// Task と context を使うとPromise.thenのような非同期処理の書き方が出来る
+            //var context = TaskScheduler.FromCurrentSynchronizationContext();
+            //Task.Run(() => GetData()).ContinueWith(x=>
+            //{
+            //    // UIスレッド上で行う処理
+            //    dataGridView1.DataSource = x.Result; //x.Result はGetData()の返り値
+            //    MessageBox.Show("完了");
 
-            }, context);
+            //}, context);
+            if (backgroundWorker1.IsBusy) {
+                MessageBox.Show("起動中です");
+            }
+            else {
+                m_DoWorkerFlag = 1;
+                backgroundWorker1.RunWorkerAsync();
+            }
+
         }
 
-        private List<DTO> GetData()
+        private List<DTO> GetData(out bool flag)
         {
+            flag = false;
             var result = new List<DTO>();
             for (int i = 0; i < 5; i++)
             {
                 System.Threading.Thread.Sleep(1000);
+                if (backgroundWorker1.CancellationPending)
+                {
+                    return result;
+                }
                 result.Add(new DTO(i.ToString(), "Name" + i));
             }
+            flag = true;
             return result;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (m_DoWorkerFlag == 1)
+            {
+                m_result = GetData(out bool flag);
+                if (flag)
+                {
+                    backgroundWorker1.ReportProgress(0, "");
+                }
+                else
+                {
+                    backgroundWorker1.ReportProgress(1, "");
+                }
+            }
+            else
+            {
+                backgroundWorker1.ReportProgress(-1, "");
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == 0)
+            {
+                dataGridView1.DataSource = m_result;
+                MessageBox.Show("完了");
+            }
+            else if (e.ProgressPercentage == 1)
+            {
+                MessageBox.Show("キャンセル");
+            }
+            else
+            {
+                MessageBox.Show("完了");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
         }
     }
 }
